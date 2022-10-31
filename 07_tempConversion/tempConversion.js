3f353f71efe1e13313dcbe082d155790b15a0bd2
Aman <amankunda9@gmail.com>
2022-10-31 16:24:11 +0530

    adding lesson 7 ans

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function (num) {
+  let temp = num - 32;
+  temp = (temp * 5) / 9;
+  const rounded = Math.round(temp * 10) / 10;
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function (num) {
+  let temp = (num * 9) / 5;
+  temp += 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
